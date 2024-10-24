1747a3634cb58cab445667d30d4e2c0d3ad86ed6
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-10-24 14:13:39 +0100

    switch to copilot

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -49,7 +49,7 @@ return {
 		local keymap = vim.keymap -- for conciseness
 
 		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" }) -- toggle file explorer on current file
+		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Open file explorer to file" }) -- open file explorer to file
 		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
 		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 	end,
