be665ad52121c1f0bfa62100ba1a04058e5a0615
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-06 19:55:08 +0000

    change ai tools

---
+++ .config/nvim/lua/custom/plugins/code-compnaion.lua
@@ -0,0 +1,15 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			strategies = {
+				chat = { adapter = "copilot" },
+				inline = { adapter = "copilot" },
+			},
+		})
+	end,
+}
