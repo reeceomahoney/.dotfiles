f9f76751e6a3f10d8cba5a56e68638a7d518a96a
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-06 21:53:39 +0000

    add code companion key map

--- .config/nvim/lua/custom/plugins/code-compnaion.lua
+++ .config/nvim/lua/custom/plugins/code-compnaion.lua
@@ -11,5 +11,15 @@ return {
 				inline = { adapter = "copilot" },
 			},
 		})
+		vim.keymap.set(
+			{ "n", "v" },
+      "<leader>a",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
 	end,
 }
