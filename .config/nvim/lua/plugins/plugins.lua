056ca3e5c73b78f2a2c11baf24e378a18a1f9d42
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-04-08 11:26:48 +0100

    add copilot

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,4 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "snacks.nvim", opts = { scroll = { enabled = false }, notifier = { enabled = false } } },
+  { "github/copilot.vim"}
 }
