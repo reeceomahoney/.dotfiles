50682e18008dcf3973dad4326574d50b41a598fb
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-07-21 13:24:46 +0100

    search ignored files by default

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,13 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "snacks.nvim", opts = { scroll = { enabled = false }, notifier = { enabled = false } } },
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+      notifier = { enabled = false },
+      picker = { sources = { files = { ignored = true } } },
+    },
+  },
   { "github/copilot.vim" },
   { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
 }
