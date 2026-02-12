c74fae6e89df1d5fce45b9563d90a41a814c208b
Reece O'Mahoney <reece.omahoney3@gmail.com>
2026-02-12 11:55:13 +0000

    update snacks.nvim config

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -5,10 +5,16 @@ return {
     opts = {
       scroll = { enabled = false },
       notifier = { enabled = false },
-      picker = { sources = { files = { ignored = true }, explorer = { ignored = true }, grep = { ignored = true } } },
+      picker = {
+        sources = {
+          files = { hidden = true, ignored = true },
+          explorer = { ignored = true },
+          grep = { hidden = true, ignored = true }
+        }
+      },
     },
   },
   { "github/copilot.vim" },
-  { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
-  { "nvim-mini/mini.pairs", enabled = false },
+  { "neovim/nvim-lspconfig",   opts = { inlay_hints = { enabled = false } } },
+  { "nvim-mini/mini.pairs",    enabled = false },
 }
